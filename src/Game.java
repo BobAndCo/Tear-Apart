d4d9c86c72a72d22d5ea316330f66d4f991ab2b9
unknown <khush8205@gmail.com>
2021-06-14 20:21:50 -0400

    fixed a lot of JAVA moments

--- src/Game.java
+++ src/Game.java
@@ -5,92 +5,116 @@ import java.io.File;
 import javax.imageio.ImageIO;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.awt.Rectangle;
 
 class Game extends JPanel {
   
-	Player player;
+  Player player;
   
-	ArrayList<Mob> mobs;
-	Biome b = new Biome("plains");
-	MyMouseListener playerMouse = new MyMouseListener();
-	PlayerController playerController = new PlayerController(player);
+  ArrayList<Mob> mobs;
+  Biome b = new Biome("plains");
   
-	Game() {
-    
-		player = new Player(300, 300, 100, 25, 10, 1, false, "player");
-    
-		mobs = new ArrayList<Mob>();
-    
-		for (int i = 0; i < 5; i++) {
-			mobs.add(new Zombie(100 + (50 * i), 100 + (75 * i)));
-		}
-    
-		b.generateBiome();
-    
-		this.addKeyListener(playerController);
-		this.addMouseListener(playerMouse);
-    
-		this.setFocusable(true);
-		this.requestFocusInWindow();
-    
-	}
+  MyMouseListener playerMouse = new MyMouseListener();
+  PlayerController playerController;
   
-	public void animate() {
-		boolean quit = false;
+  Game() {
     
-		while (!quit) {
-			this.repaint();
-		}
+    player = new Player(300, 300, 100, 25, 10, 1, false, "player");
     
-	}
+    mobs = new ArrayList<Mob>();
+    
+    for (int i = 0; i < 5; i++) {
+      mobs.add(new Zombie(100 + (50 * i), 100 + (75 * i)));
+    }
+    
+    b.generateBiome();
+     
+    playerController = new PlayerController(player);
+    this.addKeyListener(playerController);
+    
+    this.addMouseListener(playerMouse);
+    
+    this.setFocusable(true);
+    this.requestFocusInWindow();
+    
+  }
   
-	public void drawBackground(Graphics g) {
-		try {
-			File BACKGROUND_IMAGE = new File("../sprites/background.jpeg");
-			g.drawImage(ImageIO.read(BACKGROUND_IMAGE), 0, 0, this);
-		} catch (Exception e) {
-			System.out.println("Error: No Background Image Found");
-		}
-		;
-	}
+  public void animate() {
+    boolean quit = false;
+    
+    while (!quit) {
+      this.repaint();
+    }
+    
+  }
   
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		setDoubleBuffered(true);
+  public void drawBackground(Graphics g) {
+    try {
+      File BACKGROUND_IMAGE = new File("../sprites/background.jpeg");
+      g.drawImage(ImageIO.read(BACKGROUND_IMAGE), 0, 0, this);
+    } catch (Exception e) {
+      System.out.println("Error: No Background Image Found");
+    }
+    ;
+  }
+  
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    setDoubleBuffered(true);
     
-		//this.drawBackground(g);
+    //this.drawBackground(g);
     
-		b.renderBiome(g);
+    b.renderBiome(g);
     
-		b.saveBiome();
-		player.move();
-		player.draw(g);
-
-		if (playerMouse.getButtonPressed().equals("left")) {
-			b.removeBlock(playerMouse.getX(), playerMouse.getY());
-		} else if (playerMouse.getButtonPressed().equals("right")) {
-			b.addBlock(playerMouse.getX(), playerMouse.getY(), playerController.getSelectedBlock());
-		}
-
-		for (int i = 0; i < mobs.size(); i++) {
-			//System.out.println(i);
-			mobs.get(i).pathFinding(player);
-			mobs.get(i).move();
-			mobs.get(i).draw(g);
-			if (mobs.get(i).mobRect.intersects(player.x, player.y, 35, 35)){
-				//System.out.println("Ouch! " + i);
-				player.attack(mobs.get(i));
-				mobs.get(i).attack(player);
-			}
-			if (mobs.get(i).health <= 0){
-				mobs.get(i).setDead(true); 
-				mobs.remove(i);
-			}
-			if (player.health <= 0){
-				player.setDead(true); 
-			}
-		}
-		this.repaint();
-	}
+    b.saveBiome();
+    player.move();
+    player.draw(g);
+    
+    if (playerMouse.getButtonPressed().equals("left")) {
+      b.removeBlock(playerMouse.getX(), playerMouse.getY());
+    } else if (playerMouse.getButtonPressed().equals("right")) {
+      b.addBlock(playerMouse.getX(), playerMouse.getY(), playerController.getSelectedBlock());
+    }
+    
+    for (int i = 0; i < mobs.size(); i++) {
+      //System.out.println(i);
+      mobs.get(i).pathFinding(player);
+      mobs.get(i).move();
+      mobs.get(i).draw(g);
+      if (mobs.get(i).mobRect.intersects(player.x, player.y, 35, 35)){
+        //System.out.println("Ouch! " + i);
+        player.attack(mobs.get(i));
+        mobs.get(i).attack(player);
+      }
+      if (mobs.get(i).health <= 0){
+        mobs.get(i).setDead(true); 
+        mobs.remove(i);
+      }
+      if (player.health <= 0){
+        player.setDead(true); 
+      }
+    }
+    
+    Block[][] blocks = b.getMapBuffer();
+    
+    for (int i = 0; i < blocks.length; i++) {
+      for (int j = 0; j < blocks[i].length; j++) {
+        if (blocks[i][j] != null) {
+          Rectangle blockRectangle = blocks[i][j].getRect();
+          if (player.collides(blockRectangle)) {
+            player.setFalling(false);
+            player.setJumping(false);
+          }
+          for (int k = 0; k < mobs.size(); k++) {
+            if (mobs.get(k).collides(blockRectangle)) {
+              mobs.get(k).setFalling(false);
+            }
+          }
+        }
+      }
+    }
+    
+    this.repaint();
+  }
   
 }
--- src/PlayerController.java
+++ src/PlayerController.java
@@ -5,42 +5,57 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 class PlayerController implements KeyListener {
-
-	// reference to items effected by keyboard press
-	Player player;
-	private String selectedBlock = "grass";
-
-	PlayerController(Player p) {
-		player = p;
-	}
-
-	public void keyTyped(KeyEvent e) {
-		switch (e.getKeyChar()) {
-			case 'a' : player.moveLeft()        ; break;
-			case 'd' : player.moveRight()       ; break;
-			case ' ' : player.setJumping(true)  ;
-				     player.setFalling(false) ; break;
-			case '1' : selectedBlock = "grass"  ; break;
-			case '2' : selectedBlock = "dirt"   ; break;
-			case '3' : selectedBlock = "stone"  ; break;
-			case '4' : selectedBlock = "sand"   ; break;
-			//case '5' : selectedBlock = "sands"  ; break;
-			case '6' : selectedBlock = "snow"   ; break;
-			case '7' : selectedBlock = "wood"   ; break;
-			case '8' : selectedBlock = "leaf"   ; break;
-		}
-	}
-
-	public String getSelectedBlock() {
-		return selectedBlock;
-	}
-
-	public void keyPressed(KeyEvent e) {
-
-	}
-
-	public void keyReleased(KeyEvent e) {
-
-	}
-
+  
+  // reference to items effected by keyboard press
+  Player player;
+  private String selectedBlock = "grass";
+  
+  PlayerController(Player p) {
+    player = p;
+  }
+  
+  public void keyTyped(KeyEvent e) {
+    
+    char key = e.getKeyChar();
+    
+    if (key == 'a'){
+      player.moveLeft();
+    } else if (key == 'd') {
+      player.moveRight();
+    } else if (key == 'j') {
+      player.setJumping(true);
+      player.setFalling(false);
+      player.jump();
+    } else if (key == '1') {
+      selectedBlock = "grass";
+    }else if (key == '2') {
+      selectedBlock = "dirt";
+    }else if (key == '3') {
+      selectedBlock = "stone";
+    }else if (key == '4') {
+     selectedBlock = "sand";
+    } /*else if (key == '5') {
+      selectedBlock = "sands";
+    }*/else if (key == '6') {
+      selectedBlock = "snow";
+    }else if (key == '7') {
+      selectedBlock = "wood";
+    }else if (key == '8') {
+     selectedBlock = "leaf";
+    }
+    
+  }
+  
+  public String getSelectedBlock() {
+    return selectedBlock;
+  }
+  
+  public void keyPressed(KeyEvent e) {
+    
+  }
+  
+  public void keyReleased(KeyEvent e) {
+    
+  }
+  
 } // end of keyboard listener
