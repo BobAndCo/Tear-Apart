356952d65676bb9a417c153265c85daa7d04afc3
unknown <khush8205@gmail.com>
2021-06-15 22:53:28 -0400

    Just comments to the Game, Player, Mob and Player Controller class

--- src/Game.java
+++ src/Game.java
@@ -9,28 +9,43 @@ import java.awt.Rectangle;
 
 class Game extends JPanel {
   
+  //Creates a player type object
   Player player;
   
+  //2D array for the biom specfic mobs
   Mob[][] mobs;
+  
+  //Scene manager of sorts
   boolean play = false;
   boolean mainMenuLoop = true;
+  
+  //Makes an Array list of Biome type objects for dynamic number of biomes if wanted
   ArrayList<Biome> b;
   
+  //Listeners for the  different keys and scenes
+  //These are for the main game scene
   MyMouseListener playerMouse = new MyMouseListener();
-  MenuKeyListener menuKey;
   PlayerController playerController;
+  //This one is for the Menu Scene 
+  MenuKeyListener menuKey;
   
+  //To indicate what biome we are currently in 
   private int currentBiome;
+  //To indicate the max number of biomes wanted
   private int maxBiomes = 10;
   
   Game() {
     
+    //Initiates the player object
     player = new Player(300, 300, 100, 25, 10, 1, false, "player");
     
+    //Initiates the 2D Mob array
     mobs = new Mob[maxBiomes][5];
     
+    //Initiates the biome array list
     b = new ArrayList<Biome>();
     
+    //Adds the biomes to the array list
     for (int i = 0; i < maxBiomes; i++) {
       int random = (int) (Math.random() * 3);
       if (random == 0) {
@@ -42,6 +57,7 @@ class Game extends JPanel {
       }
     }
     
+    //Adds the mobs to the 2D array
     for (int i = 0; i < maxBiomes; i++) {
       for (int j = 0; j < 5; j++) {
         if (b.get(i).getType().equals("plains")) {
@@ -54,11 +70,13 @@ class Game extends JPanel {
       }
     }
     
+    //generates the first biome(also the current one)
     this.currentBiome = 0;
     for (int i = 0; i < b.size(); i++) {
       b.get(i).generateBiome();
     }
     
+    //Initiates player Controllers 
     playerController = new PlayerController(player);
     menuKey = new MenuKeyListener();
     
@@ -72,6 +90,9 @@ class Game extends JPanel {
     
   }
   
+  /**
+   * Animates the screen meaning redraws things on the screen
+   */
   public void animate() {
     boolean quit = false;
     
@@ -80,6 +101,10 @@ class Game extends JPanel {
     }
   }
   
+  /**
+   * Draws the background 
+   * @param g Is the graphics interface
+   */
   public void drawBackground(Graphics g) {
     try {
       File BACKGROUND_IMAGE = new File("../sprites/background.jpeg");
@@ -87,9 +112,13 @@ class Game extends JPanel {
     } catch (Exception e) {
       System.out.println("Error: No Background Image Found");
     }
-    ;
   }
   
+  /**
+   * Is the main drawing and checking function of the game. Firstly is draws 
+   * everything, then it checks for collisions and other things 
+   * @param g Is the graphics interface
+   */
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     setDoubleBuffered(true);
@@ -97,12 +126,12 @@ class Game extends JPanel {
     // this.drawBackground(g);
     
     if (mainMenuLoop) {
-      /* Play */
+      /* Play Button*/
       g.setColor(new Color(10,10,10));
       g.drawRect(720, 310, 480, 50);
       g.drawString("New World", 720+(480/2), 310+(50/2));
       
-      /* Exit */
+      /* Exit Button*/
       g.setColor(new Color(10,10,10));
       g.drawRect(720, 610, 480, 50);
       g.drawString("Exit", 720+(480/2), 610+(50/2));
@@ -128,68 +157,85 @@ class Game extends JPanel {
       }
       
     } else if (play) {
-      System.out.println(player.x);
       
-      if (player.x > 1420 && currentBiome != this.maxBiomes - 1) {
+      /*This is to check if the player is out of bounds and if yes changes the biomes
+       * and the player x and y position accordingly
+       */
+      if (player.x > 1890 && currentBiome != this.maxBiomes - 1) {
         System.out.println("New Biome");
         player.x = 0;
         currentBiome += 1;
         player.y = b.get(currentBiome).getFirstHeight() - 30;
       } else if (player.x < 0 && currentBiome != 0) {
-        player.x = 1420;
+        player.x = 1890;
         currentBiome -= 1;
         player.y = b.get(currentBiome).getLastHeight() - 30;
       } else if (player.x < 0 && currentBiome == 0) {
         player.x = 0;
       } else if (player.x > 1420 && currentBiome == this.maxBiomes - 1) {
-        player.x = 1450;
+        player.x = 1890;
       }
       
+      //Renders the biome
       b.get(currentBiome).renderBiome(g);
       
-      b.get(currentBiome).saveBiome();
-      
+      //Moves the player
       player.move();
+      //Draws the player
       player.draw(g);
       
+      //Checks to see with mouse button the player pressed and accordingly places or destroys blocks
       if (playerMouse.getButtonPressed().equals("left")) {
+        //Removes the block
         b.get(currentBiome).removeBlock(playerMouse.getX(), playerMouse.getY());
       } else if (playerMouse.getButtonPressed().equals("right")) {
+        //Adds a block
         b.get(currentBiome).addBlock(playerMouse.getX(), playerMouse.getY(), playerController.getSelectedBlock());
       }
       
+      //Cycles through all the mobs in the array
       for (int i = 0; i < mobs[currentBiome].length; i++) {
         // System.out.println(i);
-        
+        //Path finds the mob to the player
         mobs[currentBiome][i].pathFinding(player);
+        //Moves the mobs
         mobs[currentBiome][i].move();
+        //Draws the mobs
         mobs[currentBiome][i].draw(g);
+        //Checks if the player and mob collide
         if (mobs[currentBiome][i].collides(player.getRect())) {
           // System.out.println("Ouch! " + i);
+          //Player Attacks the mob
           player.attack(mobs[currentBiome][i]);
+          //Mob Attacks the player
           mobs[currentBiome][i].attack(player);
         }
+        //If the mobs health goes down too low, it dies
         if (mobs[currentBiome][i].health <= 0) {
           mobs[currentBiome][i].setDead(true);
         }
-        if (player.health <= 0) {
-          player.setDead(true);
-          play = false;
-          mainMenuLoop = true;
-        }
       }
       
+      //Gets the "blueprint" of all the blocks 
       Block[][] blocks = b.get(currentBiome).getMapBuffer();
       
+      //Cycles through all the blocks 
       for (int i = 0; i < blocks.length; i++) {
         for (int j = 0; j < blocks[i].length; j++) {
+          //Skips the "null" block
           if (blocks[i][j] != null) {
+            //Gets a Rectangle for the block for the collision detection
             Rectangle blockRectangle = blocks[i][j].getRect();
+            //Checks if the block collides with the player 
             if (player.collides(blockRectangle)) {
+              //Doesn't let the player fall
               player.setFalling(false);
+              //Lets the player jump
               player.setJumping(false);
             }
+            //Cycles through the mobs 
             for (int k = 0; k < mobs[currentBiome].length; k++) {
+              //Checks if the mob collided with the block
               if (mobs[currentBiome][k].collides(blockRectangle)) {
                 mobs[currentBiome][k].setFalling(false);
               }
@@ -197,9 +243,10 @@ class Game extends JPanel {
           }
         }
       }
-    } else {
+    } else { //Exits the codes
       System.exit(0); 
     }
+    //Repaints everything with updated stuff
     this.repaint();
   }
 }
--- src/Player.java
+++ src/Player.java
@@ -5,110 +5,204 @@ import javax.imageio.ImageIO;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-class Player {
+/**
+ *
+ * This class is an abstract class to represent each basic 
+ * player and its properties
+ * @author Khush Negandhi
+ * @version 1.0, May 2021 
+ * 
+ */
 
+class Player {
+  
+  //Sprite path
   String SPRITE_PATH = "../sprites/";
   String SPRITE_EXTENSION = ".jpg";
-
+  
+  //The coordinates
   public int x, y;
+  //The health
   public int health;
+  //The damage
   public int damage;
+  //The speed
   public int speed;
+  //The jump height
   public int jumpHeight;
-
+  
+  //To see if the player is dead
   private boolean isDead;
-
+  
+  //To see if the player is falling 
   private boolean falling = true;
+  //To see if the player is falling 
   private boolean jumping = false;
+  //Allows the player to move right
   private boolean movingRight = true;
+  //Allows the player to move left
   private boolean movingLeft = true;
-
+  
+  //Change in x,y corrdinates respectively
   private double dx, dy;
+  //Rectangle collision mesh 
   private Rectangle playerRect;
-
+  
+  //Sprite
   public BufferedImage sprite;
+  //Sprite name
   String spriteName;
-
+  
+  /**
+   * Creates a mob object with the specified information
+   * @param x The x coordinate of the player
+   * @param y The x coordinate of the player
+   * @param health The health of the player
+   * @param speed The speed of the player
+   * @param jumpHeight The speed of the player
+   * @param damage The attack power of the player
+   * @param dead The state of the player(dead or alive)
+   * @param spriteName The name of the sprite used by the player
+   */
   Player(int x, int y, int health, int speed, int jumpHeight, int damage, boolean dead, String spriteName) {
-
+    
     this.x = x;
     this.y = y;
-
+    
     this.health = health;
     this.damage = damage;
-
+    
     this.speed = speed;
     this.jumpHeight = jumpHeight;
-
+    
     this.dx = 0;
+    //Gravity
     this.dy = 1;
-
+    
+    //Finds the sprite and associates it with the mob
     this.sprite = loadSprite(SPRITE_PATH + spriteName + SPRITE_EXTENSION);
+    //Makes the mob rectangle
     this.playerRect = new Rectangle(x, y, 30, 30);
-
+    
     this.isDead = dead;
-
+    
     this.spriteName = spriteName;
-
+    
   }
-
+  
+  /**
+   * Returns the rectangle of the player
+   * @return the rectangle of the player
+   */
   public Rectangle getRect() {
     return playerRect;
   }
-
+  
+  /**
+   * Checks if the player collided with another Rectangle 
+   * @param Rectangle the rectangle body of teh object we want to check the player collided with
+   * @return if the player collided with a Rectangle or not
+   */
   public boolean collides(Rectangle rect) {
     if (x < rect.x + 30 && x + 30 > rect.x && y < rect.y + 30 && y + 30 > rect.y) {
       return true;
     }
     return false;
   }
-
+  
+  /**
+   * If the player is dead or not
+   * @return death state
+   */
   public boolean getDead() {
     return isDead;
   }
-
+  
+  /**
+   * Setter to set the death state of the player
+   * @param deathValue sets the falling state
+   */
   public void setDead(boolean deathValue) {
     this.isDead = deathValue;
   }
-
+  
+  /**
+   * If the player is falling or not
+   * @return falling state
+   */
   public boolean getFalling() {
     return falling;
   }
-
+  
+  /**
+   * Setter to set the falling state of the player
+   * @param fall sets the falling state
+   */
   public void setFalling(boolean fall) {
     this.falling = fall;
   }
-
+  
+  /**
+   * If the player is jumping or not
+   * @return jump state
+   */
   public boolean getJumping() {
     return jumping;
   }
-
-  public void setMoveRight(boolean right) {
-    this.movingRight = right;
-  }
-
-  public boolean getMoveRight() {
-    return movingRight;
-  }
-
-  public void setMoveLeft(boolean left) {
-    this.movingLeft = left;
-  }
-
-  public boolean getMoveLeft() {
-    return movingLeft;
-  }
-
+  
+  /**
+   * Setter to set the jump state of the player
+   * @param jump sets the jump state
+   */
   public void setJumping(boolean jump) {
     this.jumping = jump;
   }
-
-  public void attack(Mob mob) {
-
-    mob.takeDamage(this.damage);
-
+  
+  /**
+   * If the player can move right
+   * @return the ability to move right state
+   */
+  public boolean getMoveRight() {
+    return movingRight;
   }
-
+  
+  /**
+   * Setter to set the move state of the player
+   * @param right sets the jump state
+   */
+  public void setMoveRight(boolean right) {
+    this.movingRight = right;
+  }
+  
+  /**
+   * If the player can move left
+   * @return the ability to move left state
+   */
+  public boolean getMoveLeft() {
+    return movingLeft;
+  }
+  
+  /**
+   * Setter to set the move state of the player
+   * @param right sets the jump state
+   */
+  public void setMoveLeft(boolean left) {
+    this.movingLeft = left;
+  }
+  
+  /**
+   * Attacks the mob
+   * @param mpb Is the mpb
+   */
+  public void attack(Mob mob) {
+    
+    mob.takeDamage(this.damage);
+    
+  }
+  
+  /**
+   * Moves the player 
+   */
   public void move() {
     this.x = this.x + (int) this.dx;
     this.y = this.y + (int) this.dy;
@@ -122,15 +216,23 @@ class Player {
       this.dy = 0;
     }
   }
-
+  /**
+   * Sets the mobs velocity in the x direction
+   */
   public void moveRight() {
     this.dx = this.speed;
   }
-
+  
+  /**
+   * Sets the mobs velocity in the y direction
+   */
   public void moveLeft() {
     this.dx = -this.speed;
   }
-
+  
+  /**
+   * Allows the player to jump
+   */
   public void jump() {
     if (this.jumping && !this.falling) {
       this.dy = -this.jumpHeight;
@@ -138,11 +240,20 @@ class Player {
       this.falling = true;
     }
   }
-
+  
+  /**
+   * Allows the mob to take damage
+   * @param damageTaken The amount of damage inflicted
+   */
   public void takeDamage(int damageTaken) {
     this.health -= damageTaken;
   }
-
+  
+  /**
+   * Allows the mob to take damage
+   * @param damageTaken The amount of damage inflicted
+   * @return BufferedImage The image or sprite of the mob
+   */
   public BufferedImage loadSprite(String spriteName) {
     try {
       return sprite = ImageIO.read(new File(spriteName));
@@ -151,11 +262,15 @@ class Player {
     }
     return null;
   }
-
+  
+  /**
+   * Draws the mob
+   */
   public void draw(Graphics g) {
+    //Checks if the mob is dead or not
     if (!this.isDead) {
       g.drawImage(this.sprite, this.x, this.y, null);
     }
   }
-
+  
 }
